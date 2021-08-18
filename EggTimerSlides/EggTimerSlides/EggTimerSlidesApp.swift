b5e897f7883a31b9a5d5be9f9b2dd3d822aad4c3
Julia <60072112+juliabturner@users.noreply.github.com>
2021-08-18 00:24:07 -0400

    Final changes to egg Timer slides

---
+++ EggTimerSlides/EggTimerSlides/EggTimerSlidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerSlidesApp.swift
+//  EggTimerSlides
+//
+//  Created by Julia Turner on 2021-08-17.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerSlidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
